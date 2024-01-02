3c7010b01c92dcd9c5ea786c442787ee18451fdd
Anastasia <anastasia_martiniuc@homedepot.com>
2024-01-02 16:15:47 +0200

    Initial Commit

---
+++ barcode-scanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  barcode-scanner
+//
+//  Created by amartiniuc on 02.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ barcode-scanner/barcode_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_scannerApp.swift
+//  barcode-scanner
+//
+//  Created by amartiniuc on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct barcode_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
